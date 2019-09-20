b2c69e0819ab2f298b03e90fa6b78a13308be1f6
Nathan West <Lucretiel@gmail.com>
2019-09-19 21:22:29 -0700

    Many good changes!
    - Many docs and doctests
    - Removed integration tests in favor of local unit tests
    - Fixed Debug implementation for Simulation
    - Added AsRef to Simulation

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,73 @@
 //TODO: no_std
 
 //! Generations is a utility library for running generation-based simulations,
-//! such as Game of Life, other cellular automata, and genetic algorithms. Its
+//! such as [Conway's Game of Life](https://www.wikiwand.com/en/Conway%27s_Game_of_Life),
+//! other [cellular automata](https://www.wikiwand.com/en/Elementary_cellular_automaton),
+//! and [genetic algorithms](https://www.wikiwand.com/en/Genetic_algorithm). Its
 //! purpose is to make it unnecessary to allocate new model structures with
 //! each generation; instead, two generations instances are stored in a
 //! [`Generations`] struct, one of which is always considered the visible "current"
-//! generation
+//! generation.
+//!
+//! # Example
+//!
+//! In this example, our system is an array of integers. With each subsequent
+//! generation, the values in the array are the sum of the original value and
+//! its two neighbors. For simplicity, we ignore the first and last element.
+//!
+//! For example:
+//!
+//! ```text
+//! [0  1  0  1  1  -1 -1  0]
+//! [0  1  2  2  1  -1 -2  0]
+//! [0  3  5  5  2  -2 -3  0]
+//! [0  8  13 12 5  -3 -5  0]
+//! [0  21 33 30 14 -3 -8  0]
+//! ```
+//! ```
+//! use generations::Generations;
+//!
+//! // Our model type is a vector. Any type that implements the `Clearable`
+//! // trait can be used as a model.
+//! let initial_state = vec![0, 1, 0, 1, 1, -1, -1, 0];
+//!
+//! // A Generations instance stores our model, plus some scratch space to
+//! // use as the next generation each step. When stepped, the new generation is
+//! // written to this scratch space, which then becomes the current generation
+//! // while the previous generation becomes the new scratch space.
+//! let gens = Generations::new_defaulted(initial_state);
+//!
+//! // A Simulation combines a Generations instance with a rule to apply with
+//! // each step of the simulation
+//! let mut sim = gens.with_rule(move |current_gen, next_gen| {
+//!     if let Some(&first) = current_gen.first() {
+//!         next_gen.push(first);
+//!     }
+//!
+//!     for window in current_gen.windows(3) {
+//!         next_gen.push(window[0] + window[1] + window[2]);
+//!     }
+//!
+//!     if current_gen.len() > 1 {
+//!         next_gen.push(*current_gen.last().unwrap())
+//!     }
+//! });
+//!
+//! assert_eq!(sim.current(), &[0, 1, 0, 1, 1, -1, -1, 0]);
+//! sim.step();
+//! assert_eq!(sim.current(), &[0, 1, 2, 2, 1, -1, -2, 0]);
+//! sim.step();
+//! assert_eq!(sim.current(), &[0, 3, 5, 5, 2, -2, -3, 0]);
+//! sim.step();
+//! assert_eq!(sim.current(), &[0, 8, 13, 12, 5, -3, -5, 0]);
+//! sim.step();
+//! assert_eq!(sim.current(), &[0, 21, 33, 30, 14, -3, -8, 0]);
+//! ```
 
 use std::collections;
 use std::ffi;
 use std::mem;
+use std::fmt;
 
 // TODO: make this derivable
 /// [`Clearable`] is a trait for data structures that can be cleared, especially
@@ -17,12 +75,26 @@ use std::mem;
 /// [`Clearable`], and the output generation will be cleared before each step.
 ///
 /// It is implemented for all std data structures and many std types; feel free
-/// to add pull requests for any std types that you feel should be clearable
+/// to add pull requests for any std types that you think should be clearable.
 pub trait Clearable {
     // Clear this data structure, ideally without deallocating its memory.
     // Puts the data structure in a "fresh" state to be written to during each
     // new generation.
     fn clear(&mut self);
+}
+
+impl<T: Clearable> Clearable for Box<T> {
+    fn clear(&mut self) {
+        T::clear(self)
+    }
+}
+
+impl<T: Clearable> Clearable for Option<T> {
+    fn clear(&mut self) {
+        if let Some(value) = self {
+            value.clear();
+        }
+    }
 }
 
 macro_rules! clearable {
@@ -68,20 +140,6 @@ clearable! {ffi::OsString}
 // This also recursively makes each smaller tuple type clearable
 clearable! {(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P)}
 
-impl<T: Clearable> Clearable for Box<T> {
-    fn clear(&mut self) {
-        T::clear(self)
-    }
-}
-
-impl<T: Clearable> Clearable for Option<T> {
-    fn clear(&mut self) {
-        if let Some(value) = self.as_mut() {
-            value.clear();
-        }
-    }
-}
-
 /// This struct manages transitions between generations. It stores two models,
 /// one of which is considered "current" and the other "scratch". The [`step`][Generations::step]
 /// method advances the simulation by calling a function with a reference to
@@ -98,6 +156,26 @@ pub struct Generations<Model: Clearable> {
 impl<Model: Clearable> Generations<Model> {
     /// Create a new `Generations` instance with a seed model, which will
     /// become the initial current generation, and a scratch generation.
+    ///
+    /// # Example
+    ///
+    /// Create a simulation using a vector for the seed generation and a
+    /// pre-allocated vector for the scratch generation
+    ///
+    /// ```
+    /// use generations::Generations;
+    ///
+    /// let mut gen = Generations::new(
+    ///     vec![1, 2, 3, 4, 5],
+    ///     Vec::with_capacity(5)
+    /// );
+    ///
+    /// gen.step(|current_gen, next_gen| {
+    ///     assert_eq!(current_gen, &[1, 2, 3, 4, 5]);
+    ///     assert_eq!(next_gen, &[]);
+    ///     assert_eq!(next_gen.capacity(), 5);
+    /// });
+    /// ```
     #[inline]
     pub fn new(seed: Model, scratch: Model) -> Self {
         Generations {
@@ -107,7 +185,17 @@ impl<Model: Clearable> Generations<Model> {
     }
 
     /// Get a reference to the current generation. This is the result of the
-    /// most recent step or reset, or the seed generation if no steps have
+    /// most recent step or reset, or the seed generation if no steps have been
+    /// run.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use generations::Generations;
+    ///
+    /// let gen = Generations::new_defaulted(vec![1, 2, 3, 4]);
+    /// assert_eq!(gen.current(), &[1, 2, 3, 4]);
+    /// ```
     #[inline]
     pub fn current(&self) -> &Model {
         &self.current
@@ -123,6 +211,24 @@ impl<Model: Clearable> Generations<Model> {
     /// This function returns a reference to the *previously current*
     /// generation, so that it can be compared if desired with the current
     /// generation.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// // Simple example that rotates a vector 1 step
+    /// use generations::Generations;
+    ///
+    /// let mut gen = Generations::new_cloned(vec![1, 2, 3, 4]);
+    ///
+    /// let prev = gen.step(|current_gen, next_gen| {
+    ///     assert!(next_gen.is_empty());
+    ///     next_gen.extend(current_gen.iter().skip(1));
+    ///     next_gen.extend(current_gen.first());
+    /// });
+    ///
+    /// assert_eq!(prev, &[1, 2, 3, 4]);
+    /// assert_eq!(gen.current(), &[2, 3, 4, 1]);
+    /// ```
     #[inline]
     pub fn step(&mut self, stepper: impl FnOnce(&Model, &mut Model)) -> &Model {
         self.scratch.clear();
@@ -134,6 +240,17 @@ impl<Model: Clearable> Generations<Model> {
     /// Replace the current generation with a new seed generation using a
     /// function. Has no effect on the existing scratch generation. The
     /// current generation is cleared before the seed function is called.
+    ///
+    /// ```
+    /// use generations::Generations;
+    ///
+    /// let mut gen = Generations::new_defaulted(vec![1, 2, 3, 4]);
+    /// gen.reset_with(|seed_gen| {
+    ///     assert!(seed_gen.is_empty());
+    ///     seed_gen.extend(&[5, 5, 5, 5]);
+    /// });
+    /// assert_eq!(gen.current(), &[5, 5, 5, 5]);
+    /// ```
     #[inline]
     pub fn reset_with(&mut self, seeder: impl FnOnce(&mut Model)) {
         self.current.clear();
@@ -142,6 +259,17 @@ impl<Model: Clearable> Generations<Model> {
 
     /// Replace the current generation with a new seed generation. Has no
     /// effect on the existing scratch generation.
+    ///
+    /// See also [`reset_with`][Generations::reset_with] for a reset
+    /// method that reuses the existing storage of the current generation.
+    ///
+    /// ```
+    /// use generations::Generations;
+    ///
+    /// let mut gen = Generations::new_defaulted(vec![1, 2, 3, 4]);
+    /// gen.reset(vec![4, 3, 2, 1]);
+    /// assert_eq!(gen.current(), &[4, 3, 2, 1]);
+    /// ```
     #[inline]
     pub fn reset(&mut self, seed: Model) {
         self.current = seed;
@@ -183,11 +311,11 @@ impl<Model: Default + Clearable> Generations<Model> {
 /// function. It allows you to repeatedly step through generations, using
 /// the same logic with each step.
 ///
-/// It provides a [`step`] method, which advances the simulaton with the
-/// underlying stepper.
+/// It provides a [`step`][Simulation::step] method, which advances the
+/// simulaton with the underlying stepper.
 ///
 /// It is constructed with the [`Generations::with_rule`] method.
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct Simulation<Model: Clearable, Step: FnMut(&Model, &mut Model)> {
     generations: Generations<Model>,
     stepper: Step,
@@ -230,4 +358,75 @@ impl<Model: Clearable, Step: FnMut(&Model, &mut Model)> Simulation<Model, Step> 
         self.generations
     }
 }
+
+impl<Model: fmt::Debug + Clearable, Step: FnMut(&Model, &mut Model)> fmt::Debug for Simulation<Model, Step> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("Simulation")
+            .field("generations", &self.generations)
+            .field("stepper", &"<closure>")
+            .finish()
+    }
+}
+
+impl<Model: Clearable, Step: FnMut(&Model, &mut Model)> AsRef<Generations<Model>> for Simulation<Model, Step> {
+    fn as_ref(&self) -> &Generations<Model> {
+        &self.generations
+    }
+}
 // TODO: create an iterator. Probably impossible until we get GATs.
+// Conceivably we could just clone the model at every step of the iteration,
+// but that would... defeat the entire purpose of the library? unless we wrapped
+// it all up in an Rc or something.
+
+#[cfg(test)]
+mod tests {
+    use crate::Generations;
+
+    #[test]
+    fn basic_test() {
+        // For these tests, the model that we're using is a vector, where in
+        // each generation, the new value is the sum of the old value and its
+        // neighbors. For instance:
+        //
+        // [1  0  1  1  -1 -1]
+        // [1  2  2  1  -1 -2]
+        // [3  5  5  2  -2 -3]
+        // [8  13 12 5  -3 -5]
+        // [21 33 30 14 -3 -8]
+        //
+        // For simplicity of implementation of bounds checking, we use a vector
+        // bounded by zeroes to make this work
+        let gen = Generations::new(
+            vec![0, 1, 0, 1, 1, -1, -1, 0],
+            vec![],
+        );
+
+        let mut gen = gen.with_rule(|current_gen, next_gen| {
+            next_gen.push(0);
+
+            for window in current_gen.windows(3) {
+                next_gen.push(window[0] + window[1] + window[2]);
+            }
+            next_gen.push(0);
+        });
+
+        assert_eq!(gen.current(), &[0, 1, 0, 1, 1, -1, -1, 0]);
+        gen.step();
+        assert_eq!(gen.current(), &[0, 1, 2, 2, 1, -1, -2, 0]);
+        gen.step();
+        assert_eq!(gen.current(), &[0, 3, 5, 5, 2, -2, -3, 0]);
+        gen.step();
+        assert_eq!(gen.current(), &[0, 8, 13, 12, 5, -3, -5, 0]);
+        gen.step();
+        assert_eq!(gen.current(), &[0, 21, 33, 30, 14, -3, -8, 0]);
+
+        // TODO: find a way to test that no reallocations are happening
+    }
+
+    #[test]
+    fn debug_print_test() {
+        // Simulations should be debug-printable.
+        let thing = Generations::new_defaulted(vec![0]).with_rule(|_c, _n| {});
+        format!("{:?}", &thing);
+    }
+}
